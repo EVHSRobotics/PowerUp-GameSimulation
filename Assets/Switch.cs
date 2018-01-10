cd4c7469d414591354cfad31374bfd29b779beda
Kevin Palani <kevinpalani@gmail.com>
2018-01-10 09:17:39 -0800

    workingish point collection and drop off, needs more artistic stuff tho

---
+++ Assets/Switch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+	public int points = 0;
+	public movement m;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (m != null && m.hasBlock) {
+			if (m.onPlatform) {
+				m.timer -= Time.deltaTime;
+				if (m.timer < 0) {
+					m.hasBlock = false;
+					points++;
+				}
+			}
+		}
+	}
+
+
+	void OnCollisionEnter(Collision collision) {
+		print ("collision enters");
+		if (collision.gameObject.tag.Equals ("robot")) {
+			m = collision.gameObject.GetComponent<movement> ();
+			m.onPlatform = true;
+		}
+	}
+
+	void OnCollisionExit(Collision collision) {
+		print ("collision exit");
+		if (collision.gameObject.tag.Equals ("robot")) {
+			movement m = collision.gameObject.GetComponent<movement> ();
+			m.onPlatform = false;
+			m.timer = m.timerMax;
+			m = null;
+		}
+	}
+
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class movement : NetworkBehaviour {
+
+	public bool hasBlock = false;
+	public bool onPlatform = false;
+	public float timer = 3;
+	public float timerMax = 3;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!isLocalPlayer)
+			return;
+		
+
+		var x = Input.GetAxis("Horizontal")*1.5f;
+		var z = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(0, 0, z);	
+		transform.Rotate (new Vector3 (0, x, 0));
+
+		if (hasBlock) {
+			GetComponent<MeshRenderer> ().material.color = Color.magenta;
+		} else {
+			GetComponent<MeshRenderer>().material.color = Color.red;
+
+		}
+
+		if (!onPlatform) {
+			timer = timerMax;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (!hasBlock && collision.gameObject.tag.Equals ("power")) {
+			hasBlock = true;
+			Destroy (collision.gameObject);
+		}
+	}
+
+	public override void OnStartLocalPlayer(){
+		GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+}
